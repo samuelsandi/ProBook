66f9d765b60fef77f60c05fd7f65539cb62191b6
samuelsandi <samuelsandi1703@gmail.com>
2018-10-26 16:33:04 +0700

    register ajax finished

--- scripts/validate.js
+++ scripts/validate.js
@@ -23,9 +23,9 @@ function validateEmail(str){
     xhttp = new XMLHttpRequest();
     xhttp.onreadystatechange = function() {
         if (this.readyState == 4 && this.status == 200) {
-            document.getElementById("uncon").innerHTML=this.responseText;
+            document.getElementById("emcon").innerHTML=this.responseText;
         }
       };
-    xhttp.open("GET", "control/valusername.php?q="+str, true);
+    xhttp.open("GET", "control/valemail.php?q="+str, true);
     xhttp.send();
 }
